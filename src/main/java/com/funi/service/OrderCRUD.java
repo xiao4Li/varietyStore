30f59a100b7eb10f47923957e3425544cddf97a3
qi.li@funi365.com <qi.li@funi365.com>
2016-09-13 14:44:23 +0800

    dynamic pattern cglib and jdk impl

---
+++ src/main/java/com/funi/service/OrderCRUD.java
@@ -0,0 +1,22 @@
+package com.funi.service;
+
+/**
+ * Created by Administrator on 2016/9/13.
+ */
+public class OrderCRUD {
+    public void add() {
+        System.out.println("Order [ add ] method  ... ");
+    }
+
+    public void delete() {
+        System.out.println("Order [ delete ] method  ... ");
+    }
+
+    public void update() {
+        System.out.println("Order [ update ] method  ... ");
+    }
+
+    public void select() {
+        System.out.println("Order [ select ] method  ... ");
+    }
+}
---
+++ src/main/java/com/funi/tools/CGLibLogEnhance.java
@@ -0,0 +1,30 @@
+package com.funi.tools;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by Administrator on 2016/9/13.
+ */
+public class CGLibLogEnhance implements MethodInterceptor {
+    private Enhancer enhancer = new Enhancer();
+    public Object getProxy(Class clazz){
+        //设置需要创建子类的类
+        enhancer.setSuperclass(clazz);
+        enhancer.setCallback(this);
+        //通过字节码技术动态创建子类实例
+        return enhancer.create();
+    }
+    //实现MethodInterceptor接口方法
+    public Object intercept(Object obj, Method method, Object[] args,
+                            MethodProxy proxy) throws Throwable {
+        System.out.println("前置代理");
+        //通过代理类调用父类中的方法
+        Object result = proxy.invokeSuper(obj, args);
+        System.out.println("后置代理");
+        return result;
+    }
+}
